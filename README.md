1c3ededa018ef01aebbd3ff5b39e37413fde6482
Ezequiel <martinez.ezequiel42@gmail.com>
2021-08-21 20:51:12 -0700

    Finished all functionality for storing and retrieving local data to and from its respective field

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # day-planner
+
+
+
+for ( var i = 1; i < 24; i++){
+var eachLine = document.createElementById("div")
+
+
+)
+
+}
--- script.js
+++ script.js
@@ -20,29 +20,73 @@ for (var i = 1; i < 25; i++) {
     //     eachLine.html(
     // '<div class="input-group mb-3"> <span class="input-group-text" id="addon-wrapping">1AM</span> <input type="text" class="form-control" placeholder="Recipient username" aria-label="Recipient username" aria-describedby="button-addon2"> <button class="btn btn-outline-secondary saveBtn" type="button" id="button-addon2">Button</button></div>'
     //     );
-    $(".container").append('<div class="input-group mb-3 ' + hour + '" > <span class="input-group-text" id="addon-wrapping">' + hour + '</span> <input type="text" class="form-control '+ hour +'" placeholder="Recipient username" aria-label="Recipient username" aria-describedby="button-addon2"> <button class="btn btn-outline-secondary saveBtn '+ hour +'" type="button" id="button-addon2">Button</button></div>');
-}
+    $(".container").append('<div class="input-group mb-3 ' + hour + '" > <span class="input-group-text" id="addon-wrapping">' + hour + '</span> <input type="text" class="form-control ' + hour + '" placeholder="Recipient username" aria-label="Recipient username" aria-describedby="button-addon2"> <button class="btn btn-outline-secondary saveBtn ' + hour + '" type="button" id="button-addon2">Button</button></div>');
+};
+
+//using the same loop as above (could probably shorten this) iterating through each
+//of the newly created sections and setting their text to whatever has been stored previously
+for (var i = 1; i < 25; i++) {
+    var hour = i;
+    if (hour == 24) {
+        hour = "12AM";
+    }
+    else if (hour == 12) {
+        hour = hour + "PM"
+    }
+    else if (hour > 12) {
+        hour = hour - 12
+        hour = hour + "PM"
+    }
+    else {
+        hour = hour + "AM";
+    };
+
+    $("input."+hour).val(localStorage.getItem(hour));
+
+};
+
+
+
+
 //--on click run everything below
-// $("button").on("click", function(){}
-//-- hourClick, at each interval, will spit out the same thing as hour at each of
-//-- its intervals, meaning we will check for each hour class 
-// for (var C = 0; C<25; C++){
-// var hourClick = C
-// if (hourClick == 24) {
-//     hourClick = "12 AM";
-// }
-// else if (hourClick == 12) {
-//     hourClick = hourClick + "PM"
-// }
-// else if (hourClick > 12) {
-//     hourClick = hourClick - 12
-//     hourClick = hourClick + "PM"
-// }
-// else {
-//     hourClick = hourClick + "AM";
-// };
-//-- Now see if the target has a class = to hourClick
-// if (event.target === $("btn."+ hourClick)){
-// setlocalStorage("savedContent"+hourClick, $(input.hourClick))
-//  }
-//}
+$("button").on("click", function (event) {
+    console.log("clicked");
+    var target = $(event.target);
+    console.log(target);
+    //-- hourClick, at each interval, will spit out the same thing as hour at each of
+    //-- its intervals, meaning we will check for each hour class 
+    for (var C = 1; C < 25; C++) {
+        var hourClick = C
+        if (hourClick == 24) {
+            hourClick = "12AM";
+        }
+        else if (hourClick == 12) {
+            hourClick = hourClick + "PM"
+        }
+        else if (hourClick > 12) {
+            hourClick = hourClick - 12
+            hourClick = hourClick + "PM"
+        }
+        else {
+            hourClick = hourClick + "AM";
+        };
+// If target has class = hourClick, set local storag item named after hourClick 
+// and set new local storage item equal to input.hourclick.val()
+
+/*tldr: run through each number same way as first for loop, to determine which button you've clicked on
+and save the information in that buttons respective text field*/ 
+        if (target.is("." + hourClick)) {
+            console.log("got " + hourClick + "!");
+            console.log($("input."+hourClick).val());
+            localStorage.setItem(hourClick, $("input." + hourClick).val());
+        };
+
+
+        //-- Now see if the target has a class = to hourClick
+
+        // };
+
+        // setlocalStorage("savedContent"+hourClick, $(input.hourClick))
+
+    }
+});
